76ef9ed0dcc454b7de423d54472427968028f808
p242829 <p242829@dac.unicamp.br>
2023-02-16 23:05:11 -0200

    16/02/23 final aula

--- CalculadoraDescontos/CalculadoraDescontos.java
+++ CalculadoraDescontos/CalculadoraDescontos.java
@@ -1,7 +1,23 @@
 /**
  * CalculadoraDescontos
  */
+import java.util.Scanner;
 public class CalculadoraDescontos {
 
-    
+    public void calcular20(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o Preço do Produto");
+        double precoProduto = sc.nextDouble();
+        precoProduto=200;
+        final int desconto20 = 20;
+        double precoComDesconto = precoProduto - (precoProduto*desconto20/100);
+        System.out.println("O preço com Desconto é" +precoComDesconto+"reais");
+        sc.close();
+    }
+    public void calcular30(){
+        double precoProduto;
+        precoProduto=200;
+        final int desconto30 = 30;
+        double precoComDesconto = precoProduto - (precoProduto*desconto30/100);
+        System.out.println("O preço com Desconto é" +precoComDesconto+"reais");}
 }
---
+++ Operacoes/app.java
@@ -0,0 +1,9 @@
+package Operacoes;
+
+public class app {
+    public static void main(String[] args) {
+        Operadores objeto1 = new Operadores();
+        objeto1.aritmetica();
+    }
+    
+}
